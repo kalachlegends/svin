2185ae0f4e9fbdc4789b0a0f31b5ec2fdeb6d667
chudickgumanoid <sashaobrazkov1307@gmail.com>
2022-08-02 12:27:32 +0600

    design, true input

--- popup.js
+++ popup.js
@@ -1,24 +1,30 @@
-
-
 $('#ok').click(function () {
   let val = pigTransleter($('#launguagePig').val())
   copyTextClipBoard(val)
 })
-function pigTransleter(string) {
 
+function pigTransleter(string) {
   let vowels = ['а', 'и', 'е', 'ё', 'о', 'у', 'ы', 'э', 'ю', 'я']
-  function pigTransle(string, vowels) {
+  let uppers = ['А', 'И', 'Е', 'Ё', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я']
+  function pigTransle(string, vowels, uppers) {
 
     const myArray = string.split("");
 
     return myArray.reduce(function (prev, current, i, array) {
 
       vowels.forEach(element => {
-        if (element == prev) prev = "с" + element + prev
+        if (element == prev) prev = prev + "с" + element
         if (current == element) {
-
           prev = prev.substring(0, prev.length - 1)
           current = array[i - 1] + current + "с" + element
+        }
+      });
+
+      uppers.forEach(element => {
+        if (element == prev) prev = prev + "С" + element
+        if (current == element) {
+          prev = prev.substring(0, prev.length - 1)
+          current = array[i - 1] + current + "С" + element
         }
       });
 
@@ -26,8 +32,9 @@ function pigTransleter(string) {
     });
 
   }
-  return pigTransle(string, vowels)
+  return pigTransle(string, vowels, uppers)
 }
+
 function copyTextClipBoard(text) {
   navigator.clipboard.writeText(text);
 }
